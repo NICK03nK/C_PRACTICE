5e8aefdbb72eddf3063e271897292126beea26ba
ning-jiahao_NICK <nicknk579008@outlook.com>
2022-06-18 10:23:52 +0800

    NC_6_18

--- nowcoder_HJ/nowcoder_HJ/test.c
+++ nowcoder_HJ/nowcoder_HJ/test.c
@@ -6,27 +6,121 @@
 //计算字符串最后一个单词的长度，单词以空格隔开，字符串长度小于5000。（注：字符串末尾不以空格为结尾）
 //输入一行，代表要计算的字符串，非空，长度小于5000。
 //输出一个整数，表示输入字符串最后一个单词的长度。
+//
+//#include<string.h>
+//
+//int get_len(char* pc)
+//{
+//	int len = strlen(pc);
+//	char* left = pc + len - 1;
+//	char* right = pc + len - 1;
+//	while ((*(left - 1) != ' ') && (left > pc))
+//	{
+//		left--;
+//	}
+//	return right - left + 1;
+//}
+//
+//int main()
+//{
+//	char str[5000] = { 0 };
+//	str[0] = ' ';
+//	gets(str);
+//	int len = get_len(str);
+//	printf("%d\n", len);
+//	return 0;
+//}
 
-#include<string.h>
 
-int get_len(char* pc)
-{
-	int len = strlen(pc);
-	char* left = pc + len - 1;
-	char* right = pc + len - 1;
-	while ((*(left - 1) != ' ') && (left > pc))
-	{
-		left--;
-	}
-	return right - left + 1;
-}
 
-int main()
-{
-	char str[5000] = { 0 };
-	str[0] = ' ';
-	gets(str);
-	int len = get_len(str);
-	printf("%d\n", len);
-	return 0;
-}
+
+//-----------------------------------------------
+//写出一个程序，接受一个由字母、数字和空格组成的字符串，和一个字符，然后输出输入字符串中该字符的出现次数。（不区分大小写字母）
+//数据范围：1≤n≤1000
+//第一行输入一个由字母和数字以及空格组成的字符串，第二行输入一个字符。
+//输出输入字符串中含有该字符的个数。（不区分大小写字母）
+//
+//#include<string.h>
+//
+//int main()
+//{
+//	char str[1001] = { 0 };
+//	gets(str);
+//	char key = 0;
+//	scanf("%c", &key);
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < strlen(str); i++)
+//	{
+//		if (((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) && ((key >= 'a' && key <= 'z') || (key >= 'A' && key <= 'Z')))
+//		{
+//			if ((str[i] == key) || (str[i] + 32 == key) || (str[i] - 32 == key))
+//			{
+//				count++;
+//			}
+//		}
+//		else
+//		{
+//			if (str[i] == key)
+//			{
+//				count++;
+//			}
+//		}
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+
+
+
+//-----------------------------------------------
+//明明生成了NN个1到500之间的随机整数。请你删去其中重复的数字，即相同的数字只保留一个，
+//把其余相同的数去掉，然后再把这些数从小到大排序，按照排好的顺序输出。
+//数据范围：1≤n≤1000，输入的数字大小满足：1≤val≤500
+//第一行先输入随机整数的个数 N 。 接下来的 N 行每行输入一个整数，代表明明生成的随机数。
+//输出多行，表示输入数据处理后的结果
+//
+//#include<stdlib.h>
+//
+//int cmp_int(const void* e1, const void* e2)
+//{
+//	return *(int*)e1 - *(int*)e2;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[1000] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int data[1000] = { 0 };
+//	int j = 0;
+//	int k = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		int flag = 1;
+//		for (j = 0; j < k; j++)
+//		{
+//			if (data[j] == arr[i])
+//			{
+//				flag = 0;
+//				break;
+//			}
+//		}
+//		if (flag)
+//		{
+//			data[k++] = arr[i];
+//		}
+//	}
+//	qsort(data, k, sizeof(data[0]), cmp_int);
+//	for (j = 0; j < k; j++)
+//	{
+//		printf("%d\n", data[j]);
+//	}
+//	return 0;
+//}
