58e3312b7bd778e8f64b6221895b8e9e437146eb
ning-jiahao_NICK <nicknk579008@outlook.com>
2022-06-26 20:21:24 +0800

    NC_6_26

--- nowcoder_HJ/nowcoder_HJ/test.c
+++ nowcoder_HJ/nowcoder_HJ/test.c
@@ -1064,3 +1064,81 @@
 //	puts(str);
 //	return 0;
 //}
+
+
+
+
+//-----------------------------------------------
+//定义一个单词的“兄弟单词”为：交换该单词字母顺序（注：可以交换任意次），而不添加、删除、修改原有的字母就能生成的单词。
+//兄弟单词要求和原来的单词不同。例如： ab 和 ba 是兄弟单词。 ab 和 ab 则不是兄弟单词。
+//现在给定你 n 个单词，另外再给你一个单词 x ，让你寻找 x 的兄弟单词里，按字典序排列后的第 k 个单词是什么？
+//注意：字典中可能有重复单词。
+//
+//数据范围：1≤n≤1000，输入的字符串长度满足：1≤len(str)≤10，1≤k < n
+//
+//输入描述：输入只有一行。 先输入字典中单词的个数n，再输入n个单词作为字典单词。 然后输入一个单词x 最后后输入一个整数k
+//
+//输出描述：第一行输出查找到x的兄弟单词的个数m 第二行输出查找到的按照字典顺序排序后的第k个兄弟单词，没有符合第k个的话则不用输出。
+//
+//#include<string.h>
+//#include<stdlib.h>
+//
+//int cmp_str(const void* e1, const void* e2)
+//{
+//	return strcmp(*(char(*)[11])e1, *(char(*)[11])e2);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	char str[1001][11] = { 0 };
+//	char wrd[11] = { 0 };
+//	int k = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%s", str[i]);
+//	}
+//	scanf("%s", wrd);
+//	scanf("%d", &k);
+//	char bro[1001][11] = { 0 };
+//	int j = 0;
+//	int count = 0;
+//	int tmp1[26] = { 0 };
+//	for (i = 0; i < strlen(wrd); i++)
+//	{
+//		tmp1[wrd[i] - 'a']++;
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		int tmp2[26] = { 0 };
+//		for (j = 0; j < strlen(str[i]); j++)
+//		{
+//			tmp2[str[i][j] - 'a']++;
+//		}
+//		if (strcmp(str[i], wrd))
+//		{
+//			int flag = 1;
+//			for (j = 0; j < 26; j++)
+//			{
+//				if ((tmp1[j] != tmp2[j]))
+//				{
+//					flag = 0;
+//					break;
+//				}
+//			}
+//			if (flag)
+//			{
+//				strcpy(bro[count++], str[i]);
+//			}
+//		}
+//	}
+//	qsort(bro, count, sizeof(bro[0]), cmp_str);
+//	printf("%d\n", count);
+//	if (bro[k - 1][0])
+//	{
+//		printf("%s\n", bro[k - 1]);
+//	}
+//	return 0;
+//}
