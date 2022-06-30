b7f7f5d2a4fed5b6ee72f6c19134800b04721358
ning-jiahao_NICK <nicknk579008@outlook.com>
2022-06-30 19:27:04 +0800

    NC_6_30

--- nowcoder_HJ/nowcoder_HJ/test.c
+++ nowcoder_HJ/nowcoder_HJ/test.c
@@ -1602,3 +1602,97 @@
 //	}
 //	return 0;
 //}
+
+
+
+
+//-----------------------------------------------
+//有一种技巧可以对数据进行加密，它使用一个单词作为它的密匙。下面是它的工作原理：
+//首先，选择一个单词作为密匙，如TRAILBLAZERS。如果单词中包含有重复的字母，只保留
+//第1个，将所得结果作为新字母表开头，并将新建立的字母表中未出现的字母按照正常字母表顺序加入新字母表。如下所示：
+//A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+//T R A I L B Z E S C D F G H J K M N O P Q U V W X Y(实际需建立小写字母的字母表，此字母表仅为方便演示）
+//上面其他用字母表中剩余的字母填充完整。在对信息进行加密时，信息中的每个字母被固定于顶上那行，并用下面那行的对
+//应字母一一取代原文的字母(字母字符的大小写状态应该保留)。
+//因此，使用这个密匙， Attack AT DAWN(黎明时攻击)就会被加密为Tpptad TP ITVH。
+//请实现下述接口，通过指定的密匙和明文得到密文。
+//数据范围：1≤n≤100，保证输入的字符串中仅包含小写字母
+//输入描述：
+//先输入key和要加密的字符串
+//输出描述：
+//返回加密后的字符串
+//
+//#include<string.h>
+//
+//void RMV_duplicate(char* p)
+//{
+//	char tmp[27] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	int k = 0;
+//	for (i = 0; i < strlen(p); i++)
+//	{
+//		int flag = 1;
+//		for (j = 0; j < k; j++)
+//		{
+//			if (tmp[j] == p[i])
+//			{
+//				flag = 0;
+//				break;
+//			}
+//		}
+//		if (flag)
+//		{
+//			tmp[k++] = p[i];
+//		}
+//	}
+//	memset(p, 0, strlen(p));
+//	strcpy(p, tmp);
+//}
+//
+//void RMK_alphabet(char* k, char* a)
+//{
+//	int index[26] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < strlen(k); i++)
+//	{
+//		a[i] = k[i];
+//		index[k[i] - 'a']++;
+//	}
+//	int j = 0;
+//	for (j = 0; j < 26 && i < 26; j++)
+//	{
+//		if (index[j] == 0)
+//		{
+//			a[i++] = j + 'a';
+//		}
+//	}
+//}
+//
+//void Encrypt(char* s, char* a)
+//{
+//	int i = 0;
+//	for (i = 0; i < strlen(s); i++)
+//	{
+//		s[i] = a[s[i] - 'a'];
+//	}
+//}
+//
+//int main()
+//{
+//	char key[27] = { 0 };
+//	gets(key);
+//	char str[101] = { 0 };
+//	gets(str);
+//	RMV_duplicate(key);
+//	char alphabet[27] = { 0 };
+//	RMK_alphabet(key, alphabet);
+//	Encrypt(str, alphabet);
+//	puts(str);
+//	return 0;
+//}
+
+
+
+
+//-----------------------------------------------
