abb46bc3bda1e887414d5cfd3e8c051844077ba1
ning-jiahao_NICK <nicknk579008@outlook.com>
2022-07-09 23:08:15 +0800

    Assignment_5_23

--- Assignment_5_23/Assignment_5_23/test.c
+++ Assignment_5_23/Assignment_5_23/test.c
@@ -9,12 +9,38 @@
 //D选手说：C最后，我第三；
 //E选手说：我第四，A第一；
 //比赛结束后，每位选手都说对了一半，请编程确定比赛的名次
-//
-//int main()
-//{
-//	
-//	return 0;
-//}
+
+int main()
+{
+	int A = 0, B = 0, C = 0, D = 0, E = 0;
+	for (A = 0; A <= 5; A++)
+	{
+		for (B = 0; B <= 5; B++)
+		{
+			for (C = 0; C <= 5; C++)
+			{
+				for (D = 0; D <= 5; D++)
+				{
+					for (E = 0; E <= 5; E++)
+					{
+						if (   ((B == 2) + (A == 3) == 1)
+							&& ((B == 2) + (E == 4) == 1)
+							&& ((C == 1) + (D == 2) == 1)
+							&& ((C == 5) + (D == 3) == 1)
+							&& ((E == 4) + (A == 1) == 1))
+						{
+							if (A + B + C + D + E == 15 && A * B * C * D * E == 120)
+							{
+								printf("A=%d B=%d C=%d D=%d E=%d\n", A, B, C, D, E);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
 
 
 
@@ -47,30 +73,30 @@
 
 //-----------------------------------------------
 //在屏幕上打印杨辉三角
-
-int main()
-{
-	int line = 0;
-	scanf("%d", &line);
-	int arr[30][30] = { 0 };
-	int i = 0;
-	int j = 0;
-	arr[0][0] = 1;
-	for (i = 1; i < line; i++)
-	{
-		arr[i][0] = 1;
-		for (j = 1; j <= i; j++)
-		{
-			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
-		}
-	}
-	for (i = 0; i < line; i++)
-	{
-		for (j = 0; j <= i; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-	return 0;
-}
+//
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	int arr[30][30] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	arr[0][0] = 1;
+//	for (i = 1; i < line; i++)
+//	{
+//		arr[i][0] = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//		}
+//	}
+//	for (i = 0; i < line; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
