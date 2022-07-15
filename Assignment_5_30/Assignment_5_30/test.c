27cc72f6cfdc7f4152bbc98c6319b74479e71ff9
ning-jiahao_NICK <nicknk579008@outlook.com>
2022-07-15 22:07:49 +0800

    Assignment_5_30

--- Assignment_5_30/Assignment_5_30/test.c
+++ Assignment_5_30/Assignment_5_30/test.c
@@ -55,3 +55,59 @@
 //有一个数字矩阵，矩阵的每行从左到右是递增的，矩阵从上到下是递增的
 //请编写程序在这样的矩阵中查找某个数字是否存在。
 //要求：时间复杂度小于O(N)
+
+int Find_Key(int a[10][10], int x, int y, int k, int* kx, int* ky)
+{
+	int i = 0;
+	int j = y - 1;
+	while (i < x && j >= 0)
+	{
+		if (a[i][j] < k)
+		{
+			i++;
+		}
+		else if (a[i][j] > k)
+		{
+			j--;
+		}
+		else
+		{
+			*kx = i;
+			*ky = j;
+			return 1;
+		}
+	}
+	*kx = -1;
+	*ky = -1;
+	return 0;
+}
+
+int main()
+{
+	int arr[10][10] = { 0 };
+	int x = 0;
+	int y = 0;
+	scanf("%d %d", &x, &y);
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < y; j++)
+		{
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	int key = 0;
+	scanf("%d", &key);
+	int kx = -1;
+	int ky = -1;
+	if (Find_Key(arr, x, y, key, &kx, &ky))
+	{
+		printf("找到了，其下标为>>%d %d\n", kx, ky);
+	}
+	else
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
